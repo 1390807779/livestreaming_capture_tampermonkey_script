9298c02cd7acc918d91804dcd15b8cee6dd52544
1390807779 <1390807779@qq.com>
2023-03-13 17:58:48 +0800

    Creat Script

---
+++ livestream_capture_open.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         douyu livestream url catch tool
+// @namespace    1390807779@qq.com
+// @version      0.1
+// @description  Under development
+// @author       1390807779@qq.com
+// @match        https://www.douyu.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license      AGPL-3.0
+// ==/UserScript==
+
+(
+    function() {
+        'use strict';
+
+        console.log("hello");
+    }
+)();
